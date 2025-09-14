ca5ad23115b45ed8c25610cc6340a2d6b6df86d0
Jaume <jaume@nualart.cat>
2025-09-14 22:20:18 +0200

    fix: pass element id to html5-qrcode

--- backend/run.py
+++ backend/run.py
@@ -3,4 +3,4 @@ from main import app
 
 if __name__ == "__main__":
     print("--- Starting Uvicorn server from run.py ---")
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False, access_log=False)
